64692661d96ff43a4ee19018a4507ccbe4ba020c
Rahul Bandekar <rahulbandekar12@gmail.com>
2025-01-29 10:49:18 +0530

    Build and deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// filepath: /c:/Users/Rahul/LMSdashboard/vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
@@ -12,19 +13,15 @@ export default defineConfig({
       plugins: [tailwindcss, autoprefixer],
     },
   },
-
   server: {
     host: '0.0.0.0', // Allows access from the local network
     port: 5173,      // Default port
     allowedHosts: ['pd53yj-5173.csb.app']
   },
-
   build: {
     rollupOptions: {
       input: {
-        // Externalize dependencies that should not be bundled
         main: 'src/main.jsx',
-        // Add other entry points if needed
       },
       external: ['react-chartjs-2'],
     },
